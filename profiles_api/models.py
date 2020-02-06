e287226430b44063805ecd920e4cd476e21ed84b
unknown <subhambanerjee138@gmail.com>
2020-02-06 17:14:44 +0530

    Added Hello API view

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,13 +7,13 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """ Manager for user profiles """
 
-    def create_user(self, email, name, password = None):
+    def create_user(self, email, name, password=None):
         """ Create a new user profile """
         if not email:
-            raise ValueError (' User must have an email address')
+            raise ValueError(' User must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email = email, name = name)
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
 
@@ -22,24 +22,24 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, name, password):
-
         """ Create a new superuser """
 
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
-        user.is_staff = True 
+        user.is_staff = True
 
         user.save(using=self._db)
 
         return user
 
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """ Database Model for user profile """
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
@@ -48,12 +48,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def get_full_name(self):
         """ Retrieve Full name of User """
-        return self.name 
+        return self.name
+
     def get_short_name(self):
         """ Retrieve Short name of User """
         return self.name
 
     def __str__(self):
         """ Return a string representation of this User """
-        return self.email    
-
+        return self.email
